8bf6662258700490fc45eb9dc0125722160cf4ba
Richmond Antor <antorgm2@gmail.com>
2025-07-04 00:36:59 +0600

    Create Find Number From String.cpp

---
+++ Find Number From String.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+   string S;
+   cin>>S;
+   for(int i =0; i< S.length(); i++){
+
+if (S[i] == '0' || S[i] == '1' || S[i] == '2' || S[i] == '3' || 
+    S[i] == '4' || S[i] == '5' || S[i] == '6' || S[i] == '7' || 
+    S[i] == '8' || S[i] == '9')
+cout<<S[i];
+
+   }
+
+    return 0;
+}
